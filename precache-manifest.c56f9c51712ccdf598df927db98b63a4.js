5488a3988db2ab88cd81f8fb8312665b909c6c91
Aaron Cook <cookx770@umn.edu>
2019-01-12 11:49:25 -0600

    Adding the static data

---
+++ precache-manifest.c56f9c51712ccdf598df927db98b63a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc312f88c02286fa4eb",
+    "url": "/art-portfolio-react/static/js/runtime~main.1fc312f8.js"
+  },
+  {
+    "revision": "01470c587859e63a67e6",
+    "url": "/art-portfolio-react/static/js/main.01470c58.chunk.js"
+  },
+  {
+    "revision": "72f3c5d62e55ebe0dba2",
+    "url": "/art-portfolio-react/static/js/1.72f3c5d6.chunk.js"
+  },
+  {
+    "revision": "01470c587859e63a67e6",
+    "url": "/art-portfolio-react/static/css/main.8dbbec05.chunk.css"
+  },
+  {
+    "revision": "72f3c5d62e55ebe0dba2",
+    "url": "/art-portfolio-react/static/css/1.b2c0836d.chunk.css"
+  },
+  {
+    "revision": "f5be59ef1d4b4de41822835d234632b3",
+    "url": "/art-portfolio-react/index.html"
+  }
+];
